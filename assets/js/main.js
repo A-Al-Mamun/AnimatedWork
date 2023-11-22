49ccde4c05cf1488972b96b0711c9a44d53065ed
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2023-11-22 18:35:42 +0600

    Perfectly done the slide

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,29 @@ let preBtn = document.getElementsByClassName('left-btn')[0];
 
 let nextBtn = document.getElementsByClassName('right-btn')[0];
 
-let slider = document.querySelectorAll('.slider');
+let slider = document.querySelector('.slider');
+let slideItem = document.querySelectorAll('.item');
 
-slider.forEach(item => {
-    // console.log(slider[item])
-    nextBtn.addEventListener('click', () => {
-        // item.style.display = 'block';
-        console.log(item);
-    });
-});
+let slideIndex = 0;
+slideItem[slideIndex].classList.add('active');
+
+nextBtn.addEventListener('click', () => {
+    slideItem[slideIndex].classList.remove('active');
+
+    slideIndex = (slideIndex === slideItem.length - 1) ? 0 : slideIndex + 1;
+    slideItem[slideIndex].classList.add('active');
+
+    slider.style.transform = `translateX(-${slideIndex * 100}%)`;
+})
+
+
+preBtn.addEventListener('click', () => {
+    slideItem[slideIndex].classList.remove('active');
+
+    slideIndex = (slideIndex === 0) ? slideItem.length - 1 : slideIndex - 1;
+    slideItem[slideIndex].classList.add('active');
+
+    slider.style.transform = `translateX(-${slideIndex * 100}%)`;
+})
+
 
